843887160228c6a7af174e207380cb9bd9646eb6
jsebasct <jsebasct@gmail.com>
2020-05-17 13:33:14 -0300

    test class created

--- src/test/java/org/example/javatest/util/StringUtilTest.java
+++ src/test/java/org/example/javatest/util/StringUtilTest.java
@@ -1,19 +1,13 @@
 package org.example.javatest.util;
 
 import org.junit.Assert;
+import org.junit.Test;
 
 public class StringUtilTest {
 
-    public static void main(String[] args) {
-
+    @Test
+    public void testRepeat() {
         Assert.assertEquals("holaholahola", StringUtil.repeat("hola", 3));
-
         Assert.assertEquals("hola", StringUtil.repeat("hola", 1));
     }
-
-//    private static void assertEquals(String actual, String expected) {
-//        if (!actual.equals(expected)) {
-//            throw new RuntimeException(actual + " no es igual a lo esperado: " + expected);
-//        }
-//    }
 }
