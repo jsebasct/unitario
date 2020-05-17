f5f9e1df4af441b9332bc2c0272e63af05d74f4c
jsebasct <jsebasct@gmail.com>
2020-05-17 13:07:17 -0300

    feat: add junit 4

--- src/test/java/org/example/javatest/util/StringUtilTest.java
+++ src/test/java/org/example/javatest/util/StringUtilTest.java
@@ -1,29 +1,19 @@
 package org.example.javatest.util;
 
-//import org.junit.After;
-
-//import static org.junit.Assert.*;
+import org.junit.Assert;
 
 public class StringUtilTest {
 
-//    @After
-//    public void tearDown() throws Exception {
-//    }
-
     public static void main(String[] args) {
-        String res = StringUtil.repeat("hola", 3);
-        assertEquals(res, "holaholahola");
 
+        Assert.assertEquals("holaholahola", StringUtil.repeat("hola", 3));
 
-        String res1 = StringUtil.repeat("hola", 1);
-        if (!res1.equals("hola")) {
-            throw new RuntimeException("error");
-        }
+        Assert.assertEquals("hola", StringUtil.repeat("hola", 1));
     }
 
-    private static void assertEquals(String actual, String expected) {
-        if (!actual.equals(expected)) {
-            throw new RuntimeException(actual + " no es igual a lo esperado: " + expected);
-        }
-    }
+//    private static void assertEquals(String actual, String expected) {
+//        if (!actual.equals(expected)) {
+//            throw new RuntimeException(actual + " no es igual a lo esperado: " + expected);
+//        }
+//    }
 }
