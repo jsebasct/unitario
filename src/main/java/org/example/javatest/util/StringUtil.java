aba4c7915d2c9cdf5bad98ab613c6ac3d9111951
jsebasct <jsebasct@gmail.com>
2020-05-17 14:11:05 -0300

    feat: test edge cases

--- src/main/java/org/example/javatest/util/StringUtil.java
+++ src/main/java/org/example/javatest/util/StringUtil.java
@@ -3,6 +3,9 @@ package org.example.javatest.util;
 public class StringUtil {
 
     public static String repeat(String str, int times) {
+
+        if (times < 0) throw new IllegalArgumentException("No se admiten valores negativos");
+
         String result = "";
         for (int i = 0; i < times; i++) {
             result += str;
--- src/test/java/org/example/javatest/util/StringUtilTest.java
+++ src/test/java/org/example/javatest/util/StringUtilTest.java
@@ -6,8 +6,22 @@ import org.junit.Test;
 public class StringUtilTest {
 
     @Test
-    public void testRepeat() {
-        Assert.assertEquals("holaholahola", StringUtil.repeat("hola", 3));
+    public void testRepeatOnce() {
         Assert.assertEquals("hola", StringUtil.repeat("hola", 1));
+    }
+
+    @Test
+    public void testRepeatMultiple() {
+        Assert.assertEquals("holaholahola", StringUtil.repeat("hola", 3));
+    }
+
+    @Test
+    public void testRepeatZero() {
+        Assert.assertEquals("", StringUtil.repeat("hola", 0));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testRepeatNegativeTimes() {
+        Assert.assertEquals("", StringUtil.repeat("hola", -1));
     }
 }
